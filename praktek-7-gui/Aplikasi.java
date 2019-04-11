f52790fb1f1bcf293611a1eff1fa86e912f46134
tamami <tamami.oka@gmail.com>
2019-04-11 16:02:44 +0700

    event: ok

--- praktek-7-gui/Aplikasi.java
+++ praktek-7-gui/Aplikasi.java
@@ -1,7 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class Aplikasi {
+
+    private static JTextField txtNim;
+    private static JTextField txtNama;
+    private static JTextField txtKelas;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame();
@@ -9,28 +14,33 @@ public class Aplikasi {
         JPanel panel = new JPanel();
         panel.setLayout(new GridLayout(3, 2));
         JButton btn = new JButton("Proses");
+        btn.addActionListener(new BtnClick());
         JLabel lblNim = new JLabel("NIM");
         JLabel lblNama = new JLabel("NAMA");
         JLabel lblKelas = new JLabel("KELAS");
-        JTextField txtNim = new JTextField();
-        JTextField txtNama = new JTextField();
-        JTextField txtKelas = new JTextField();
+        txtNim = new JTextField();
+        txtNama = new JTextField();
+        txtKelas = new JTextField();
 
         panel.add(lblNim); panel.add(txtNim);
         panel.add(lblNama); panel.add(txtNama);
         panel.add(lblKelas); panel.add(txtKelas);
 
-        //frame.getContentPane().add(label, BorderLayout.CENTER);
         frame.getContentPane().add(panel, BorderLayout.CENTER);
         frame.getContentPane().add(btn, BorderLayout.SOUTH);
-        //frame.getContentPane().add(new JLabel("utara"), BorderLayout.NORTH);
-        //frame.getContentPane().add(new JLabel("selatan"), BorderLayout.SOUTH);
-        //frame.getContentPane().add(new JLabel("timur"), BorderLayout.EAST);
-        //frame.getContentPane().add(new JLabel("barat"), BorderLayout.WEST);
         frame.pack();
-        //frame.setSize(400, 300);
+        frame.setMinimumSize(new Dimension(300, 200));
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    static class BtnClick implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            JOptionPane.showMessageDialog(null,
+                    txtNim.getText() + " - " +
+                    txtNama.getText() + " - " +
+                    txtKelas.getText());
+        }
     }
 
 }
